9c46908c8e0e4913d7ab6189b6f4c744b430fded
Richard Liao <liao098@gmail.com>
2020-03-03 00:26:47 +0000

    add 3_queries_group-by

---
+++ 3_queries_group-by/1_assignments_per_day.sql
@@ -0,0 +1,8 @@
+SELECT
+  day,
+  COUNT(id) AS total_assignments
+FROM assignments
+GROUP BY
+  day
+ORDER BY
+  day;
---
+++ 3_queries_group-by/2_busy_days.sql
@@ -0,0 +1,10 @@
+SELECT
+  day,
+  COUNT(id) AS total_assignments
+FROM assignments
+GROUP BY
+  day
+HAVING
+  count(id) >= 10
+ORDER BY
+  day;
